80cba7680e8b795bd8bab0eb60a367bc1f9dac18
Jan Vlietland <j.vlietland@search4solutions.nl>
2025-04-28 14:07:20 +0200

    vite fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-plugin-tsconfig-paths';
 
 export default defineConfig({
+  base: '/analyser/',  
   plugins: [tsconfigPaths()],
   resolve: {
     alias: {
